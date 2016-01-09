864156231e882f1c77861e074400f643f9484d4b
Mansur Ashraf <mansoor.ashraf@gmail.com>
2016-01-08 23:51:39 -0800

    added Calculator.java and Pom.xml

--- src/main/java/com/anas/Calculator.java
+++ src/main/java/com/anas/Calculator.java
@@ -5,4 +5,38 @@ package com.anas;
  * @since 1/8/16
  */
 public class Calculator {
+
+    public int add(int a, int b) {
+
+    }
+
+    public int subtract(int a, int b) {
+
+    }
+
+    public int multiply(int a, int b) {
+
+    }
+
+    /**
+     * rule 1 => b cannot be 0
+     * rule 2  must return int
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public int divide(int a, int b) {
+
+    }
+
+
+    public static void main(String[] args) {
+        final Calculator calculator = new Calculator();
+
+        System.out.println("2 + 2 = " + calculator.add(2, 2));
+        System.out.println("2 - 2 = " + calculator.subtract(2, 2));
+        System.out.println("2 * 2 = " + calculator.multiply(2, 2));
+        System.out.println("2 divided by 2 = " + calculator.divide(2, 2));
+    }
 }
