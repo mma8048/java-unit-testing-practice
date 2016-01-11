e7200bc6327883415df1c7dda08bfcb4a912a20b
ASHAHID <anasshahid@hotmail.com>
2016-01-10 20:41:16 -0600

    All the functions added. Still need to work on divide part !

--- src/main/java/com/anas/Calculator.java
+++ src/main/java/com/anas/Calculator.java
@@ -1,42 +1,44 @@
 package com.anas;
 
-/**
+/*
  * @author Mansur Ashraf
  * @since 1/8/16
  */
+
+
 public class Calculator {
 
     public int add(int a, int b) {
-           return a + b;
+        return a + b;
     }
 
-    /*public int subtract(int a, int b) {
-
+    public int subtract(int a, int b) {
+        return a - b;
     }
-
     public int multiply(int a, int b) {
-
+        return a * b;
     }
 
-    *//**
+
+    /*
      * rule 1 => b cannot be 0
      * rule 2  must return int
      *
      * @param a
      * @param b
      * @return
-     *//*
+     */
+
     public int divide(int a, int b) {
-
-    }*/
-
+        return a / b;
+    }
 
     public static void main(String[] args) {
         final Calculator calculator = new Calculator();
 
         System.out.println("2 + 2 = " + calculator.add(2, 2));
-       /* System.out.println("2 - 2 = " + calculator.subtract(2, 2));
+       System.out.println("2 - 2 = " + calculator.subtract(2, 2));
         System.out.println("2 * 2 = " + calculator.multiply(2, 2));
-        System.out.println("2 divided by 2 = " + calculator.divide(2, 2));*/
+        System.out.println("2 divided by 2 = " + calculator.divide(2, 2));
     }
 }
