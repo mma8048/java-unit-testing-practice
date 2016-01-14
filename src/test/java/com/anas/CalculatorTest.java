f0f3a9774dbbbe2d9be82d3efea7d32f7b4a7d0c
Mansur Ashraf <mansoor.ashraf@gmail.com>
2016-01-14 07:25:51 -0800

    added unit tests

--- src/test/java/com/anas/CalculatorTest.java
+++ src/test/java/com/anas/CalculatorTest.java
@@ -21,10 +21,10 @@ public class CalculatorTest {
 
     @Test
     public void testAddition() {
-        assertEquals(2.0, calculator.add(1.0, 1.0), 0.0);
-        assertEquals(0.0, calculator.add(-1.0, 1.0), 0.0);
-        assertEquals(1.0, calculator.add(0, 1.0), 0.0);
-        assertEquals(1.0, calculator.add(1.0, 0), 0.0);
+        assertEquals("Incorrect result from adding two numbers", 2.0, calculator.add(1.0, 1.0), 0.0);
+        assertEquals("Incorrect result from adding positive and negative numbers", 0.0, calculator.add(-1.0, 1.0), 0.0);
+        assertEquals("Addition with 0 didnt not return correct result", 1.0, calculator.add(0, 1.0), 0.0);
+        assertEquals("Addition with 0 didnt not return correct result", 1.0, calculator.add(1.0, 0), 0.0);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -32,5 +32,34 @@ public class CalculatorTest {
         calculator.add(Double.MAX_VALUE, Double.MAX_VALUE);
     }
 
+    // Please implement these
+    @Test
+    public void testSubtract() throws Exception {
 
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiplyAll() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
 }
